ded64c63d51dbb438f6cc57fdc7e2d25f7f59591
Abdullah khan <abdullahadil108@gmail.com>
2025-10-20 22:26:50 +0500

    Update searchengine.cpp

--- searchengine.cpp
+++ searchengine.cpp
@@ -2,13 +2,9 @@
 #include<fstream>
 #include<sstream>
 #include<map>
-#include<set>
-#include<unordered_map>
-#include<iomanip>
 #include<algorithm>
 #include<vector>
 #include<cmath>
-#include<cctype>
 #include<filesystem>
 using namespace std;
 namespace fs = filesystem;
@@ -78,12 +74,71 @@ void buildIndexes(string folderpath){
 
 //TF-IDF query channel
 
+map<int , double> queryEngine(string query){
+    vector<string> qToken= tokenize(query);
+    map<int, double> scores;
 
+    for(string term: qToken){
+        if(invertedIndex.find(term)==invertedIndex.end()){
+            continue;
+        }
+        int df= invertedIndex[term].size(); //number of docs containing searched terms
+        double idf=log((double)totaldoc/(df+1));//inverse of doc frequency
+
+        for(auto[docID,freq] : invertedIndex[term]){
+            double tf= (double)freq/docLength[docID];
+            double tfidf=tf*idf;
+            scores[docID]+=tfidf;
+        }
+    }
+
+    return scores;
+
+}
+
+//display of result
+
+void displayResult(map<int, double> scores){
+    vector<pair<int,double>> result(scores.begin(), scores.end());
+
+    for(int i=0; i<result.size(); i++){
+        auto key=result[i];
+        int j=i-1;
+
+        while(j>=0 && result[j].second<key.second){
+            result[j+1]=result[j];
+            j--;
+        }
+        result[j+1]=key;
+
+    }
+    if(result.empty()){
+        cout<<"No file found. "<<endl;
+        return;
+    }
+    cout<<"Search Result: ";
+    for(auto & p: result){
+        cout<<"Doc "<<p.first<<" Score: "<<p.second<<endl;
+    }
+}
 
 
 
 
 int main(){
+    string folderPath;
+    buildIndexes(folderPath);
+
+    while(true){
+        string query;
+        cout<<"Enter search query : ";
+        getline(cin,folderPath);
+        if(query=="exit")break;
+
+        map<int,double> scores=queryEngine(query);
+        displayResult(scores);
+    }
+
 
 return 0;
 }
